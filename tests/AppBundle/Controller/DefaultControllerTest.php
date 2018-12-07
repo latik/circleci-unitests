9dd82191e3d8e7119c41289fb606b76f28fef00d
Latik <latkovskyi@gmail.com>
2018-12-07 09:34:08 +0200

    Use Liip

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = $this->makeClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->isSuccessful($client->getResponse());
+
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 
