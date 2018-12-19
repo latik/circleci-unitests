3d882a35f3869735646da639481fdbe25ed8ce2e
Latik <latkovskyi@gmail.com>
2018-12-19 13:02:50 +0200

    Add test for fixtures

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,13 @@
 namespace Tests\AppBundle\Controller;
 
 use AppBundle\DataFixtures\AppFixture;
+use AppBundle\Repository\UserRepository;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $this->loadFixtures([
-            AppFixture::class,
-        ]);
-
         $client = $this->makeClient();
 
         $crawler = $client->request('GET', '/');
@@ -20,6 +17,17 @@ class DefaultControllerTest extends WebTestCase
         $this->isSuccessful($client->getResponse());
 
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    public function testFixtures()
+    {
+        $this->loadFixtures([
+            AppFixture::class,
+        ]);
+
+        $userRepo = $this->getContainer()->get(UserRepository::class);
+
+        $this->assertCount(19, $userRepo->findAll());
     }
 
     public function testSkip()
